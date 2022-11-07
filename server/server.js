bacc5cfd5b71122489c47db5d1b60013c556f94c
casatoo <casato6666@gmail.com>
2022-11-07 21:55:42 +0900

    id, pw 세션 저장

--- server/server.js
+++ server/server.js
@@ -2,20 +2,47 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 4000;
+const session = require("express-session");
 
 const DB = {
-  user: [],
+  user: [
+    {
+      id: "asd",
+      pw: "asd",
+    },
+    {
+      id: "asd1",
+      pw: "asd1",
+    },
+    {
+      id: "asd2",
+      pw: "asd2",
+    },
+  ],
 };
 
 app.use(express.json());
 app.use(
+  session({
+    secret: "SECRET",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(
   cors({
     origin: true,
+    credentials: true,
   })
 );
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/test", (req, res) => {
+  console.log(req.session);
+  res.send("?");
 });
 
 app.post("/join", (req, res) => {
@@ -30,6 +57,22 @@ app.post("/join", (req, res) => {
     code: "success",
     message: "회원가입되었습니다.",
   });
+});
+
+app.post("/login", (req, res) => {
+  const { id, pw } = req.body;
+
+  const user = DB.user;
+
+  const findUser = user.find((item) => {
+    return item.id === id && item.pw === pw;
+  });
+  /**
+   * 세션 저장
+   */
+  req.session.loginUser = findUser;
+  req.session.save();
+  res.send(findUser);
 });
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
+
+axios.defaults.withCredentials = true;
 
 function Main() {
   return <div>메인페이지</div>;
 }
 
 function Login() {
-  return <div>login</div>;
+  const [user, setUser] = React.useState({
+    id: "",
+    pw: "",
+  });
+  const 로그인 = async () => {
+    await axios({
+      method: "POST",
+      url: "http://127.0.0.1:4000/login",
+      data: user,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  const 데이터변경 = (event) => {
+    const name = event.target.name;
+    const cloneUsers = { ...user };
+    cloneUsers[name] = event.target.value;
+
+    setUser(cloneUsers);
+  };
+
+  return (
+    <div>
+      <input type="text" name="id" onChange={데이터변경} />
+      <input type="password" name="pw" onChange={데이터변경} />
+      <button type="button" onClick={로그인}>
+        로그인
+      </button>
+    </div>
+  );
 }
 function Join() {
+  const navigation = useNavigate();
   const [users, setUser] = React.useState({
     id: "",
     pw: "",
@@ -22,10 +59,15 @@ function Join() {
       url: "http://127.0.0.1:4000/join",
       data: users,
     })
-      .then(() => {})
+      .then((res) => {
+        const { code, message } = res.data;
+        console.log(code);
+        console.log(message);
+      })
       .catch((e) => {
         console.log(e);
       });
+    navigation("/login");
   };
 
   const 데이터변경 = (event) => {
